b4f067d43b6cc9cc0ef71b0d4041e71940ccfb8a
Hillary Yeraldy Carpio Morales <yeraldycarpio_morales@hotmail.com>
2024-03-09 13:55:59 -0600

    Agregar archivos de proyecto.

---
+++ Practica0903202401/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica0903202401.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
